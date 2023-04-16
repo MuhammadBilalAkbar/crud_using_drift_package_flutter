f28d2ca783f90719d2b6bcf07fc963bafd8ec0fd
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-04-16 23:22:20 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'route/route_generator.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Drift CRUD Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          titleTextStyle: TextStyle(fontSize: 25),
+        ),
+        textTheme: const TextTheme(
+          labelLarge: TextStyle(fontSize: 25),
+        ),
+      ),
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,114 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+import '/data/local/db/app_db.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late AppDb db;
+
+  List<EmployeeData> employeesForUi = [];
+  List<EmployeeData> employeeFromApi = [];
+
+  @override
+  void initState() {
+    super.initState();
+    db = AppDb();
+    SystemChannels.textInput.invokeMethod('TextInput.hide');
+  }
+
+  Future<List<EmployeeData>> refreshUsersFromApi() async {
+    final employeesFromApi = await db.getEmployees();
+    setState(() {
+      employeesForUi = employeesFromApi;
+    });
+    return employeesForUi;
+  }
+
+  @override
+  void dispose() {
+    db.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('CRUD Operations With Drift Flutter'),
+        ),
+        body: RefreshIndicator(
+          onRefresh: refreshUsersFromApi,
+          // onRefresh: db.getEmployees,
+          child: FutureBuilder<List<EmployeeData>>(
+            future: db.getEmployees(),
+            builder: (context, snapshot) {
+              final employees = snapshot.data;
+              if (snapshot.connectionState != ConnectionState.done) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(snapshot.error.toString()),
+                );
+              }
+              if (employees != null) {
+                return ListView.builder(
+                  itemCount: employees.length,
+                  itemBuilder: (context, index) {
+                    final employee = employees[index];
+                    return GestureDetector(
+                      onTap: () => Navigator.pushNamed(
+                        context,
+                        '/edit_employee',
+                        arguments: employee.id,
+                      ),
+                      child: Card(
+                        shape: const RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Colors.green,
+                              style: BorderStyle.solid,
+                              width: 1.2,
+                            ),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(16.0),
+                              bottomRight: Radius.circular(16.0),
+                            )),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(employee.id.toString()),
+                              Text(employee.userName),
+                              Text(employee.firstName),
+                              Text(employee.lastName),
+                              Text(employee.dateOfBirth.toString()),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }
+              return const Text('No data found');
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () => Navigator.pushNamed(context, '/add_employee'),
+          icon: const Icon(Icons.add),
+          label: const Text('Add Employee'),
+        ),
+      );
+}
